91f617150b107b5bf4300fcd6f61bc667fdcdc42
AlvinChernobyl <motoblur.connect@gmail.com>
2025-07-18 12:52:56 -0300

    Adicionando testde de unidade

--- projeto-final/src/test/java/neocamp_teamcubation/projeto_final/service/EstadioServiceTest.java
+++ projeto-final/src/test/java/neocamp_teamcubation/projeto_final/service/EstadioServiceTest.java
@@ -1,5 +1,7 @@
 package neocamp_teamcubation.projeto_final.service;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EstadioServiceTest {
